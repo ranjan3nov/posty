91d6214d1dc19ae29cc1a6f4d24cd7d79c76f476
ranjan3nov <ranjan3nov@gmail.com>
2023-07-06 14:15:14 +0530

    Initial Commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            @auth
+                <form action="{{ route('posts') }}" method="post" class="mb-4">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="body" class="sr-only">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="4"
+                            class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
+                            placeholder="Post something!"></textarea>
+
+                        @error('body')
+                            <div class="text-red-500 mt-2 text-sm">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+                    </div>
+                </form>
+            @endauth
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    {{-- <x-post :post="$post" /> --}}
+                    <div class="mb-4">
+                        <a href="{{ route('user.posts', $post->user) }}" class="font-bold">{{ $post->user->username }}</a>
+                        <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+
+                        <p class="mb-2">{{ $post->body }}</p>
+                        @auth
+                            @can('delete', $post)
+                                <div>
+                                    <form action="{{ route('posts.destory', $post) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="text-blue-500">Delete</button>
+                                    </form>
+                                </div>
+                            @endcan
+                        @endauth
+                        <div class="flex items-center">
+                            @auth
+                                @if (!$post->likedBy(auth()->user()))
+                                    <form action="{{ route('posts.likes', $post) }}" class="mr-1" method="POST">
+                                        @csrf
+                                        <button class="text-blue-500">Like</button>
+                                    </form>
+                                @else
+                                    <form action="{{ route('posts.likes', $post) }}" class="mr-1" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="text-blue-500">Unlike</button>
+                                    </form>
+                                @endif
+                            @endauth
+                            <span>
+                                {{ $post->likes->count() }}
+                                {{ Str::plural('like', $post->likes->count()) }}
+                            </span>
+                        </div>
+                    </div>
+                @endforeach
+                {{-- Pagination --}}
+                {{-- {{ $posts->links() }} --}}
+            @else
+                <p>There are no posts</p>
+            @endif
+        </div>
+    </div>
+@endsection
